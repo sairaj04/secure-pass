cd49fdd08188fa845f0478ea3d1cc85df9911b6a
hima234 <himasri7862@gmail.com>
2024-03-17 12:37:54 +0530

    initial commit

---
+++ src/components/Announcement.tsx
@@ -0,0 +1,5 @@
+export default function Announcement() {
+  return (
+    <div className="border-b flex text-center justify-center h-12 items-center px-6"></div>
+  );
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="h-14 w-full flex text-center border-t items-center justify-center">
+      <h1 className="">
+        Created by{" "}
+        <a href="#">
+          <span className="font-semibold">Hima Sri</span>
+        </a>
+      </h1>
+    </footer>
+  );
+}
